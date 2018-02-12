27e35d4c2a4c214adbbe17d2fa9186b0f1fa0928
codascii <houmadi.bacar.mohamed@gmail.com>
2018-02-12 22:12:38 +0100

    Ajout des messages d'erreur et de succès

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
 include 'Codraz.php';
-//Commented by Dark Folt
 $key = "";
 
 if(isset($_POST['btc'])) {
 	if(isset($_POST['text']) AND !empty($_POST['text'])) {
 		$key = htmlentities(Codraz::genKey(20));
 		$result = htmlentities(Codraz::crypt($_POST['text'], $key));
-		//$decrypt = Codraz::decrypt(htmlspecialchars_decode($result), $key);
+		$_CODRAZ['SUC'] = "Message crypter avec succès.";
+	} else {
+		$_CODRAZ['ERR'] = "Veuillez taper un message à crypter.";
 	}
 } elseif (isset($_POST['btd'])) {
 	if(isset($_POST['text']) AND !empty($_POST['text']) AND isset($_POST['key']) AND !empty($_POST['key'])) {
 		$key = htmlentities($_POST['key']);
 		$result = Codraz::decrypt(htmlspecialchars_decode($_POST['text']), $key);
+		$_CODRAZ['SUC'] = "Message décrypter avec succès.";
+	} else {
+		$_CODRAZ['ERR'] = "Aucun message à décrypter.";
 	}
 } else {
-	echo "Erreur de commande !!";
+	// Pas de traitement...
 }
 
 ?>
@@ -25,6 +29,8 @@ if(isset($_POST['btc'])) {
 		<meta charset="utf-8"/>
 		<title>Codraz</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<script type="text/javascript" language="javascript">
             window.onload = function() {
                 var key = document.getElementById("r_key");
@@ -49,6 +55,8 @@ if(isset($_POST['btc'])) {
 							<input type="text" name="key" id="r_key" value="<?= $key ?>" class="form-control"/><p>&nbsp;</p>
 						</div>
 						<p>&nbsp;</p>
+						<?php if(isset($_CODRAZ['ERR'])) { echo '<div class="alert alert-danger alert-dismissable"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.$_CODRAZ['ERR'].'</div>'; unset($_CODRAZ['ERR']); } ?>
+						<?php if(isset($_CODRAZ['SUC'])) { echo '<div class="alert alert-success alert-dismissable"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.$_CODRAZ['SUC'].'</div>'; unset($_CODRAZ['SUC']);} ?>
 						<form method="post" action="#">
 							<h4>Tapez votre message ici :</h4>
 							<textarea class="form-control" name="text" style="padding:20px;width:100%; height:100px;border: 1px solid #dedede;border-radius: 10px; background-color: white; border-bottom-left-radius: 0px; border-bottom-right-radius: 0px;" placeholder="Votre message..."></textarea>
