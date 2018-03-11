17a9cc6e2334c34c6f57371ad73525704ca05518
codascii <houmadi.bacar.mohamed@gmail.com>
2018-03-11 17:24:37 +0100

    Code de chiffrage

---
+++ chiffrer.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+include 'Codraz.php';
+
+//	Génération de la clé de chiffrage
+$key = htmlentities(Codraz::genKey(20));
+
+// Chiffrage du texte
+$_SESSION['inout'] = htmlentities(Codraz::crypt($_SESSION['inout'], $key));
+$_SESSION['key'] = $key;
+$_CODRAZ['SUC'] = "Message crypter avec succès.";
+
+// Redirection à la page d'accueil
+header("Location: /codraz");
