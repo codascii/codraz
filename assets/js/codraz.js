e88aa4a68cbbbfc772c26f56c3a66fffe93946a8
Codascii <dev.codascii@gmail.com>
2020-12-05 10:50:16 +0100

    Suppression de la requête ajax

--- assets/js/codraz.js
+++ assets/js/codraz.js
@@ -5,23 +5,23 @@
 *************************************/
 
 // La page est chargé
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 	///////////////////////////////////////////////////////
 	// 				Gestion des boutons radio
 	///////////////////////////////////////////////////////
 	var actions = document.querySelectorAll("div.radio");
 
 	for (var i = actions.length - 1; i >= 0; i--) {
-		actions[i].addEventListener("click", function(e) {
+		actions[i].addEventListener("click", function (e) {
 			// On effectue un traitement que si le bouton n'est pas déjà cocher
-			if(!this.classList.contains("checked")) {
+			if (!this.classList.contains("checked")) {
 				// Suppression de la class "checked" pour l'autre radio
 				document.querySelector("div.radio.checked").classList.remove("checked");
 
 				// Ajout de la class "checked" au bouton radio cliqué
 				this.classList.add("checked");
 
-				if(this.classList.contains("chiffrer")) {
+				if (this.classList.contains("chiffrer")) {
 					document.querySelector("input#chiffrer").click();
 					document.getElementById("bt").value = "Chiffrer";
 				} else {
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Gestion du click sur les labels
 	var labels = document.querySelectorAll("div#action label");
 	for (var i = labels.length - 1; i >= 0; i--) {
-		labels[i].addEventListener("click", function(e) {
+		labels[i].addEventListener("click", function (e) {
 			// Quand on click sur le label
 			// On va chercher le div associé pour déclencher un click
 			document.getElementById(this.htmlFor).click();
@@ -50,11 +50,11 @@ document.addEventListener("DOMContentLoaded", function() {
 	var lightBtn = document.getElementById('light-btn');
 	var img = document.getElementById('codraz-title');
 
-	darkBtn.addEventListener('click', function(e) {
+	darkBtn.addEventListener('click', function (e) {
 		if (body.classList.contains('light')) {
 			//	On commance par changer l'image
 			img.src = "images/codraz-dark.png";
-			
+
 			lightBtn.classList.remove('active');
 			darkBtn.classList.add('active');
 
@@ -63,71 +63,18 @@ document.addEventListener("DOMContentLoaded", function() {
 		}
 	});
 
-	lightBtn.addEventListener('click', function(e) {
+	lightBtn.addEventListener('click', function (e) {
 		if (body.classList.contains('dark')) {
 			//	On commance par changer l'image
 			img.src = "images/codraz-light.png";
-			
+
 			darkBtn.classList.remove('active');
 			lightBtn.classList.add('active');
-			
+
 
 			body.classList.add('light');
 			body.classList.remove('dark');
 		}
 	});
-
-	var btChifferDechiffrer = document.getElementById('bt');
-	btChifferDechiffrer.addEventListener('click', function (event) {
-		event.preventDefault();
-		const keyInput = document.getElementById('key');
-		const key = keyInput.value;
-		const messageArea = document.querySelector('textarea')
-		const message = messageArea.value;
-		const actions = document.getElementsByName('action');
-		const action = actions[0].checked ? actions[0].value : actions[1].value;
-		console.log(action);
-		
-
-		console.log(key + ' ... ' + message);
-
-		// Ajax Request
-		var xhr = getXMLHttpRequest();
-
-        xhr.open("POST", "control.php");
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.send('key=' + key + '&inout=' + message + '&bt=&action=' + action);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-				var response = JSON.parse(xhr.responseText);
-
-				if (!response.error) {
-					keyInput.value = response.key;
-					messageArea.value = response.message;
-				}
-            }
-        };
-	});
-
 });
 
-function getXMLHttpRequest() {
-    var xhr = null;
-
-    if (window.XMLHttpRequest || window.ActiveXObject) {
-        if (window.ActiveXObject) {
-            try {
-                xhr = new ActiveXObject("Msxml2.XMLHTTP");
-            } catch(e) {
-                xhr = new ActiveXObject("Microsoft.XMLHTTP");
-            }
-        } else {
-            xhr = new XMLHttpRequest();
-        }
-    } else {
-        alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
-        return null;
-    }
-
-    return xhr;
-}
